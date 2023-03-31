a4871b207926fa79b20d9ccb1a340752b941f906
Kalinin Alexey <leshakalinin93@gmail.com>
2023-03-31 10:39:38 +0300

    added getmetrics functions

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,79 +1,105 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
 	"strconv"
 	"strings"
 )
 
 type MemStorage struct {
-	gaugeMap   map[string]float64
-	counterMap map[string]int64
+	GaugeMap   map[string]float64
+	CounterMap map[string]int64
 }
 
 func (ms *MemStorage) Init() {
-	(*ms).gaugeMap = make(map[string]float64)
-	(*ms).counterMap = make(map[string]int64)
+	(*ms).GaugeMap = make(map[string]float64)
+	(*ms).CounterMap = make(map[string]int64)
 }
 
-func (ms *MemStorage) addGauge(key string, value float64) {
-	ms.gaugeMap[key] = value
+func (ms *MemStorage) AddGauge(key string, value float64) {
+	ms.GaugeMap[key] = value
 }
 
-func (ms *MemStorage) addCounter(key string, value int64) {
-	ms.counterMap[key] += value
+func (ms *MemStorage) AddCounter(key string, value int64) {
+	ms.CounterMap[key] += value
+}
+
+func (ms *MemStorage) GetGauge(key string) (float64, error) {
+	value, ok := (*ms).GaugeMap[key]
+	if ok {
+		return value, nil
+	}
+	return 0, errors.New("Missing key " + key)
+}
+
+func (ms *MemStorage) GetCounter(key string) (int64, error) {
+	value, ok := (*ms).CounterMap[key]
+	if ok {
+		return value, nil
+	}
+	return 0, errors.New("Missing key " + key)
 }
 
 var MS MemStorage
 
 func updateGauge(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		http.Error(w, "Only post method required", http.StatusMethodNotAllowed)
+		http.Error(w, "Not a valid HTTP method.", http.StatusMethodNotAllowed)
 		return
 	}
 	if r.Header.Get("Content-Type") != "text/plain" {
-		http.Error(w, "Content-Type must be text/plain", http.StatusUnsupportedMediaType)
+		http.Error(w, "Not a valid content-type.", http.StatusUnsupportedMediaType)
 		return
 	}
 
 	urlSplit := strings.Split(r.URL.String(), "/")
 	if len(urlSplit) != 5 {
-		http.Error(w, "Wrong URL", http.StatusBadRequest)
+		http.Error(w, "Not a valid URL.", http.StatusBadRequest)
 		return
 	}
 
 	key := urlSplit[len(urlSplit)-2]
 	value, err := strconv.ParseFloat(urlSplit[len(urlSplit)-1], 64)
 	if err != nil {
-		panic(err)
+		http.Error(w, "Not a valid metric value.", http.StatusBadRequest)
+		return
 	}
-	MS.addGauge(key, value)
+	MS.AddGauge(key, value)
 	w.Header().Set("content-type", "text/plain; charset=utf-8")
+	gaugeValue, err := MS.GetGauge(key)
+	response := fmt.Sprintf("Gauge value updated. Metric named %s is now %5f.", key, gaugeValue)
+	w.Write([]byte(response))
 }
 
 func updateCounter(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		http.Error(w, "Only post method required", http.StatusMethodNotAllowed)
+		http.Error(w, "Not a valid HTTP method.", http.StatusMethodNotAllowed)
 		return
 	}
 	if r.Header.Get("Content-Type") != "text/plain" {
-		http.Error(w, "Content-Type must be text/plain", http.StatusUnsupportedMediaType)
+		http.Error(w, "Not a valid content-type.", http.StatusUnsupportedMediaType)
 		return
 	}
 
 	urlSplit := strings.Split(r.URL.String(), "/")
 	if len(urlSplit) != 5 {
-		http.Error(w, "Wrong URL", http.StatusBadRequest)
+		http.Error(w, "Not a valid URL.", http.StatusBadRequest)
 		return
 	}
 
 	key := urlSplit[len(urlSplit)-2]
 	value, err := strconv.ParseInt(urlSplit[len(urlSplit)-1], 10, 64)
 	if err != nil {
-		panic(err)
+		http.Error(w, "Not a valid metric value.", http.StatusBadRequest)
+		return
 	}
-	MS.addCounter(key, value)
+	MS.AddCounter(key, value)
 	w.Header().Set("content-type", "text/plain; charset=utf-8")
+	counterValue, err := MS.GetCounter(key)
+	response := fmt.Sprintf("Counter value updated. Metric named %s is now %d.", key, counterValue)
+	w.Write([]byte(response))
 }
 
 func main() {
