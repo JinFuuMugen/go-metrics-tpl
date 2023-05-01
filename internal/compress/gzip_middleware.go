680a5703877d45a3f280be45dcee6d5d2fbd488f
Alexey Kalinin <leshakalinin93@gmail.com>
2023-05-02 01:36:59 +0300

    fixed content-encoding header

--- internal/compress/gzip_middleware.go
+++ internal/compress/gzip_middleware.go
@@ -8,7 +8,7 @@ import (
 
 func GzipMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
+		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
 			if strings.Contains(r.Header.Get("Content-Type"), "application/json") || strings.Contains(r.Header.Get("Content-Type"), "text/html") {
 				gz, err := gzip.NewReader(r.Body)
 				if err != nil {
@@ -18,6 +18,7 @@ func GzipMiddleware(next http.Handler) http.Handler {
 				defer gz.Close()
 				r.Body = gz
 				r.Header.Del("Content-Length")
+				r.Header.Set("Content-Encoding", "gzip")
 			}
 		}
 		next.ServeHTTP(w, r)
