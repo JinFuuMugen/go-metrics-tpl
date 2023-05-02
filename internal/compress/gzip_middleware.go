41003951a5cb28882db308ee7e582cb15ff30e65
Alexey Kalinin <leshakalinin93@gmail.com>
2023-05-02 18:32:43 +0300

    added encoding to middleware

--- internal/compress/gzip_middleware.go
+++ internal/compress/gzip_middleware.go
@@ -12,27 +12,47 @@ func GzipMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
 			if strings.Contains(r.Header.Get("Content-Type"), "application/json") || strings.Contains(r.Header.Get("Content-Type"), "text/html") {
-				//Decode body
 				reader, err := gzip.NewReader(r.Body)
 				if err != nil {
-					//Internal server error
 					http.Error(w, `internal server error`, http.StatusInternalServerError)
 					return
 				}
 				defer reader.Close()
 				decodedBody, err := io.ReadAll(reader)
 				if err != nil {
-					//Internal server error
 					http.Error(w, `internal server error`, http.StatusInternalServerError)
 					return
 				}
 				r.Body = io.NopCloser(bytes.NewBuffer(decodedBody))
 			} else {
-				//Bad request
 				http.Error(w, `invalid content type for gzip encoding`, http.StatusBadRequest)
 				return
 			}
 		}
-		next.ServeHTTP(w, r)
+
+		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			w.Header().Set("Content-Encoding", "gzip")
+
+			gzipWriter := gzip.NewWriter(w)
+			defer gzipWriter.Close()
+
+			gzipResponseWriter := &gzipResponseWriter{ResponseWriter: w, Writer: gzipWriter}
+			next.ServeHTTP(gzipResponseWriter, r)
+		} else {
+			next.ServeHTTP(w, r)
+		}
 	})
+}
+
+type gzipResponseWriter struct {
+	http.ResponseWriter
+	io.Writer
+}
+
+func (w gzipResponseWriter) Write(b []byte) (int, error) {
+	return w.Writer.Write(b)
+}
+
+func (w gzipResponseWriter) Flush() {
+	w.ResponseWriter.(http.Flusher).Flush()
 }
