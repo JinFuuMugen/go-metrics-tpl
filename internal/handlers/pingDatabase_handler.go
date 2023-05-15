d8afc8ffe9754c442d1cd641826b77b91c57f597
Alexey Kalinin <leshakalinin93@gmail.com>
2023-05-15 23:38:17 +0300

    changed defer close

--- internal/handlers/pingDatabase_handler.go
+++ internal/handlers/pingDatabase_handler.go
@@ -13,10 +13,10 @@ func PingDBHandler(cfg *config.ServerConfig) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		dsn := cfg.DatabaseDSN
 		db, err := sql.Open("pgx", dsn)
-		defer db.Close()
 		if err != nil {
 			logger.Errorf("can't connect to database: %s", err)
 			http.Error(w, "can't connect to database", http.StatusInternalServerError)
 		}
+		defer db.Close()
 	}
 }
