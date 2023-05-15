980d5d1e458fafd843744b274518fe15845f2aac
Alexey Kalinin <leshakalinin93@gmail.com>
2023-05-16 00:35:15 +0300

    updated ping database handler

--- internal/handlers/pingDatabase_handler.go
+++ internal/handlers/pingDatabase_handler.go
@@ -1,12 +1,14 @@
 package handlers
 
 import (
+	"context"
 	"database/sql"
 	"github.com/JinFuuMugen/go-metrics-tpl.git/internal/config"
 	"github.com/JinFuuMugen/go-metrics-tpl.git/internal/logger"
 	_ "github.com/jackc/pgx/v5"
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"net/http"
+	"time"
 )
 
 func PingDBHandler(cfg *config.ServerConfig) http.HandlerFunc {
@@ -16,7 +18,18 @@ func PingDBHandler(cfg *config.ServerConfig) http.HandlerFunc {
 		if err != nil {
 			logger.Errorf("can't connect to database: %s", err)
 			http.Error(w, "can't connect to database", http.StatusInternalServerError)
+			return
 		}
 		defer db.Close()
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err = db.PingContext(ctx); err != nil {
+			logger.Errorf("error pinging database: %s", err)
+			http.Error(w, "error pinging database", http.StatusInternalServerError)
+			return
+		} else {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
 	}
 }
