87b57c39cb64888b744d797bb03f7c0645d4da6a
Alexey Kalinin <leshakalinin93@gmail.com>
2023-06-13 05:51:55 +0300

    test batch handler

--- internal/handlers/batchUpdateMetrics_handler.go
+++ internal/handlers/batchUpdateMetrics_handler.go
@@ -27,6 +27,7 @@ func UpdateBatchMetricsHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, fmt.Sprintf("cannot process body: %s", err), http.StatusBadRequest)
 		return
 	}
+
 	for _, metric := range metrics {
 		switch metric.MType {
 		case storage.MetricTypeCounter:
@@ -55,16 +56,16 @@ func UpdateBatchMetricsHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	jsonBytes, err := json.Marshal(metrics)
-	if err != nil {
-		logger.Errorf("cannot serialize metrics to json: %s", err)
-		http.Error(w, fmt.Sprintf("internal server error: %s", err), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	_, err = w.Write(jsonBytes)
-	if err != nil {
-		logger.Fatalf("cannot write response: %s", err)
-	}
+	//jsonBytes, err := json.Marshal(metrics)
+	//if err != nil {
+	//	logger.Errorf("cannot serialize metrics to json: %s", err)
+	//	http.Error(w, fmt.Sprintf("internal server error: %s", err), http.StatusInternalServerError)
+	//	return
+	//}
+	//
+	//w.Header().Set("Content-Type", "application/json")
+	//_, err = w.Write(jsonBytes)
+	//if err != nil {
+	//	logger.Fatalf("cannot write response: %s", err)
+	//}
 }
