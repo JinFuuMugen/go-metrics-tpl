b9b5e50ec283314d1b8b59e17a0f20536c42e1cf
Kalinin Alexey <leshakalinin93@gmail.com>
2023-06-21 15:57:32 +0300

    added B-Tree index type

--- internal/database/psql.go
+++ internal/database/psql.go
@@ -19,7 +19,7 @@ func InitDatabase(config string) error {
 
 	ctx := context.Background()
 
-	_, err = DB.ExecContext(ctx, "CREATE TABLE IF NOT EXISTS metrics (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), type TEXT NOT NULL, value DOUBLE PRECISION, delta INT, name TEXT NOT NULL); CREATE UNIQUE INDEX IF NOT EXISTS idx_metrics_type_name ON metrics (type, name);")
+	_, err = DB.ExecContext(ctx, "CREATE TABLE IF NOT EXISTS metrics (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), type TEXT NOT NULL, value DOUBLE PRECISION, delta INT, name TEXT NOT NULL); CREATE UNIQUE INDEX IF NOT EXISTS idx_metrics_type_name ON metrics USING btree (type, name);")
 
 	if err != nil {
 		return fmt.Errorf("cannot create table: %w", err)
